8fc3dd7fc75a34a1b3d9829e4aa7844fcd17c1e9
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-02-19 23:24:00 +0530

    Update Create_table.sql

--- Create_table.sql
+++ Create_table.sql
@@ -1,2 +1,6 @@
 Create table cust
 (cust_id varchar2(3) primary key, lname varchar2(15), fname varchar2(15) not null, area varchar2(2), phone_no number(8));
+
+create table movie
+( mv_no varchar(2) primary key, title varchar2(25),
+type varchar2(10), star varchar2(25), price number(8,2));
