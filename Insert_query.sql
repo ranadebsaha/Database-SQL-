9e51e9f2c5c0c9e4cd6eec70d621d421c86ab1b8
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-02-20 20:15:22 +0530

    Create Insert_query.sql

---
+++ Insert_query.sql
@@ -0,0 +1,16 @@
+Insert into cust
+values
+('a01', 'Bayross', 'Ivan', 'sa', 6125467);
+
+insert into cust
+values('a02', 'Saitwal', 'Vandana', 'mu', 5560379);
+
+insert into cust
+values('a03', 'Jaguste', 'Pramada', 'da', 4563891);
+
+insert into cust
+values('a04', 'Navindgi', 'Basu', 'ba', 6125401);
+
+insert into cust (cust_id, lname, fname, area) values ('a05', 'Sreedharan', 'Ravi', 'va');
+
+insert into cust (cust_id, fname, area, phone_no) values ('a06', 'Rukmini', 'gh',5125274);
