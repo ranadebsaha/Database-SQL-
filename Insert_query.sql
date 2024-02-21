487d48db0cbe7f948554aaef8e6caeb2b37846fc
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-02-21 23:31:04 +0530

    Update Insert_query.sql

--- Insert_query.sql
+++ Insert_query.sql
@@ -14,3 +14,35 @@ values('a04', 'Navindgi', 'Basu', 'ba', 6125401);
 insert into cust (cust_id, lname, fname, area) values ('a05', 'Sreedharan', 'Ravi', 'va');
 
 insert into cust (cust_id, fname, area, phone_no) values ('a06', 'Rukmini', 'gh',5125274);
+
+
+insert into movie values
+( '1','bloody vengeance', 'action', 'jackie chan', 180.95)
+
+
+insert into movie values
+( '2','the firm','thriller','tom cruise', 200.00)
+
+insert into movie values
+( '3','pretty woman','romance','richard gere', 150.55)
+
+insert into movie values
+( '4','home alone','comedy','macaulay culkin', 150.00)
+
+insert into movie values
+( '5','the fugitive','thriller','harisson ford', 200.00)
+
+insert into movie values
+( '6','coma','suspense','michael douglas', 100.00)
+
+insert into movie values
+( '7','dracula','horror','gary oldman', 150.25)
+
+insert into movie values
+( '8','quick change','comedy','bill muray', 100.00)
+
+insert into movie values
+( '9','gone with the wind','drama','clark gable', 200.00)
+
+insert into movie values
+( '10','carry on doctor','comedy','leslie phillips', 100.00)
