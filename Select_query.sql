b416fe34d2f3b53689bbc8fcc068a98615cc9633
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-04-21 00:52:38 +0530

    Update Select_query.sql

--- Select_query.sql
+++ Select_query.sql
@@ -29,3 +29,4 @@
 29.A. select 'The Invoice No of customer ID ' || invoice.cust_id || ' is ' || inv_no || ' and Movie No. is ' || invoice.mv_no from cust, movie, invoice where cust.cust_id=invoice.cust_id and movie.mv_no= invoice.mv_no 
 29.B. select invoice.cust_id|| ' has taken Movie No. '|| invoice.mv_no ||' on ' || issue_date || ' and will return on '|| return_date from cust, movie, invoice where cust.cust_id=invoice.cust_id and movie.mv_no= invoice.mv_no
 30. SELECT avg(price), type FROM movie GROUP BY type
+31. SELECT COUNT(mv_no), type FROM movie GROUP BY type
