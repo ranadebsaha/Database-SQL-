a68acf4d61ac3a2f352e1b94df51c219a97bb8ca
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-06-20 23:53:40 +0530

    Update Select_query.sql

--- Select_query.sql
+++ Select_query.sql
@@ -34,3 +34,4 @@
 33. select avg(price),type from movie where price<=150 group by type 
 34. SELECT avg(price), type FROM movie where price>=150 and (type='thriller' or type='comedy') GROUP BY type select * from cust
 35. select mv_no from invoice inner join cust on invoice.cust_id=cust.cust_id and fname='Ivan'
+36. select fname, lname, mv_no from invoice inner join cust on invoice.cust_id= cust.cust_id
