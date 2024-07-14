c25e17b09a20e380e5fc0fd57eeacadc9ce3a501
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-07-14 15:20:47 +0530

    Update Select_query.sql

--- Select_query.sql
+++ Select_query.sql
@@ -43,3 +43,4 @@
 42. select fname , lname ,area from cust where cust_id= (select cust_id from invoice where inv_no='i10')
 43. select fname ,lname, phone_no from cust where cust_id in (select cust_id from invoice where issue_date <'01-AUG-93')
 44. select title from movie where mv_no in (select mv_no from invoice where cust_id in ( select cust_id from cust where fname= 'Vandana' or fname ='Ivan'))
+45.  select mv_no , title from movie where mv_no in(select mv_no from invoice where cust_id in (select cust_id from cust))
