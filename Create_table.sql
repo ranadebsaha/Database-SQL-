20b31a2ae3400f2a0f0fff8a541252f9855737bc
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2025-01-07 23:26:35 +0530

    Update Create_table.sql

--- Create_table.sql
+++ Create_table.sql
@@ -7,3 +7,6 @@ type varchar2(10), star varchar2(25), price number(8,2));
 
 create table invoice
 ( inv_no varchar2(3) primary key, mv_no varchar(2) references movie, cust_id varchar2(3) references cust, issue_date date, return_date date);
+
+create table student_details
+  ( sid char(6) primary key, name varchar(25), address varchar(35), branch char(5), adm_date date, check(branch in ('cse', 'it', 'ee','ece','mca','bca')), check(adm_date>to_date('01-06-2002','DD-MM-YYYY')));
