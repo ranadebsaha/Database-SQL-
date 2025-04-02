b4c1a71c9d60121cdadcf224ed301718bd699886
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2025-04-02 09:39:58 +0530

    Update Select_query.sql

--- Select_query.sql
+++ Select_query.sql
@@ -58,3 +58,4 @@ f. select sid from student_details where adm_date like '__-___-02';
 g. select m_sub1+m_sub2+m_sub3 total_marks from student_marks where sid='s0005';
 h. select sid from student_marks where m_sub2<45
 i. select sid from student_marks where m_sub1<45 and m_sub3<45
+j. select sid from student_marks where m_sub2<45 and (m_sub1>=45 and m_sub3>=45)
